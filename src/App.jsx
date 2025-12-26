94e70b344d78238b2d54d4a0b6e469c123eaa32e
hawra <hawra.yusuf02@outlook.com>
2025-12-26 14:48:21 +0300

    added product list into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-// src/App.jsx
-
 import { Routes, Route } from 'react-router'; // Import React Router
 
 import NavBar from './components/NavBar/NavBar';
-// Import the SignUpForm component
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
+import ProductList from './components/ProductList/ProductList';
 import { useContext } from 'react';
 import { UserContext } from './contexts/UserContext';
 
@@ -23,10 +21,7 @@ const App = () => {
           user ?
           <>
             <Route path='/' element={<Dashboard/>}/>
-            <Route path='/products' element={<h1>Producs</h1>}/>
-            <Route path='/favs' element={<h1>Favs</h1>}/>
-            <Route path='/profile' element={<h1>{user.username}</h1>}/>
-            <Route path='/orders' element={<h1>ORDERS</h1>}/>
+            <Route path='/products' element={<ProductList />}/>
           </>
             :
             <Route path='/' element={<Landing/>}/>
